d217c12f8295a2246888e220f8541ad2fe09cbbf
adam-kafia <adem.kafia@esprit.tn>
2022-02-25 18:51:57 +0100

    fixed web port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
-	const port = process.env.port;
+	const port = process.env.PORT;
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
 	app.useGlobalInterceptors(new TransformInterceptor());
