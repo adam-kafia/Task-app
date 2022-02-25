14a79a9222c49998490c5bfc7609bdfe2f3bca6b
adam-kafia <adem.kafia@esprit.tn>
2022-02-25 16:12:22 +0100

    Added port env var

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
+	const port = process.env.port;
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
 	app.useGlobalInterceptors(new TransformInterceptor());
 	const config = new DocumentBuilder().setTitle('Task Management App').build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
-	await app.listen(3000);
+	await app.listen(port || 3000);
 }
 bootstrap();
